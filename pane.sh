17588646ca3adb62da467609d278c85765aadc34
Dany Gagnon <ddanygagnon@gmail.com>
2024-02-18 16:34:59 -0500

    feat: floating term is better

--- pane.sh
+++ pane.sh
@@ -1,30 +1,39 @@
 #/bin/bash
 
-TMP_DIR=/tmp/pane
+# TMP_DIR=/tmp/pane
+#
+# current_dir=$(tmux display-message -p -F "#{pane_current_path}" 2> /dev/null)
+# default_dir=$current_dir
+# echo $current_dir > $TMP_DIR
+#
+# while [ "$current_dir" != "/" ]; do
+#     if [ -d "$current_dir/.git" ]; then
+#         break
+#     fi
+#     current_dir=$(dirname "$current_dir")
+# done
+#
+# if [ "$current_dir" == "/" ]; then
+#     current_dir=$default_dir
+# fi
+#
+# tmux new-session -d -s scratch
+# if [ $(tmux list-panes | wc -l) -eq 1 ]; then
+#     tmux join-pane -p 35 -s scratch
+#     tmux kill-pane -t scratch.1
+#     tmux new-session -d -s scratch
+#
+#     if [ $(tmux display-message -p -F "#{pane_current_path}" 2> /dev/null) != $current_dir ]; then
+#         tmux send-keys "cd $current_dir" C-m 2> /dev/null
+#     fi
+# else
+#     tmux join-pane -t scratch.1
+# fi
 
-current_dir=$(tmux display-message -p -F "#{pane_current_path}")
-default_dir=$current_dir
-echo $current_dir > $TMP_DIR
-
-while [ "$current_dir" != "/" ]; do
-    if [ -d "$current_dir/.git" ]; then
-        break
-    fi
-    current_dir=$(dirname "$current_dir")
-done
-
-if [ "$current_dir" == "/" ]; then
-    current_dir=$default_dir
-fi
-
-if [ $(tmux list-panes | wc -l) -eq 1 ]; then
-    tmux new-session -d -s scratch 2> /dev/null
-    tmux join-pane -p 35 -s scratch
-    # if not already current dir cd
-    if [ $(tmux display-message -p -F "#{pane_current_path}") != $current_dir ]; then
-        tmux send-keys "cd $current_dir" C-m
-    fi
-else
-    tmux new-session -d -s scratch 2> /dev/null
-    tmux move-pane -t scratch
-fi
+if [ "${PANE_COUNT}" = 1 ]; then
+    tmux split-window -c "#{pane_current_path}"
+  elif [ -n "${PANE_ZOOMED}" ]; then
+    tmux select-pane -t:.-
+  else
+    tmux resize-pane -Z -t1
+  fi
